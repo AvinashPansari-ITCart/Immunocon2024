f6a37a3f36bcc4f27fcb64d85a9717e2bee9ef61
numandotdev <numandotdev@gmail.com>
2024-01-30 17:23:41 +0530

    added the countdown timer section

--- scripts/logo.slider.js
+++ scripts/logo.slider.js
@@ -1,5 +1,3 @@
 const logoSlides = document.querySelector('.logo_slides');
 const DupliLogoSlides = logoSlides.cloneNode(true);
 logoSlides.parentNode.appendChild(DupliLogoSlides)
-// const DupliLogoSlides1 = logoSlides.cloneNode(true);
-// logoSlides.parentNode.appendChild(DupliLogoSlides1)
